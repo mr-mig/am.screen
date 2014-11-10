61a4e39ca9960dda7d5ec6e71c162ed1f982022f
Mr_Mig <alexey.migutsky@gmail.com>
2014-11-10 23:28:44 +0100

    Basic screen implementation

--- angular.js
+++ angular.js
@@ -2,4 +2,21 @@
 var angular = require('angular-cjs');
 var createScreen = require('./index');
 
-module.exports = angular.module('', []);
+module.exports = function (definition) {
+  var result = createScreen(definition);
+
+  var module = angular.module(result.moduleName, result.moduleDependencies);
+
+  module.config(function ($routeProvider, ChannelsProvider) {
+    $routeProvider
+      .when(result.route, {
+        template: result.template,
+        controller: result.screenName
+      });
+
+    ChannelsProvider.createChannels(result.channels);
+  });
+
+  return module
+    .controller(result.screenName, result.controller);
+};
--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 'use strict';
 var conv = require('am.convention');
 
-module.exports = function createScreen(){
+var definitionSchema = {
+  name: 'element name',
+  ngDeps: ['array', 'of', 'angular', 'module names'],
+  controller: function (injectable, services, go, here) {
+  },
+  route: '/screen/url/address',
+  template: 'template',
+  channels: []
+};
 
+
+module.exports = function createScreen(definition) {
+  var deps = definition.ngDeps || [];
+  definition.controller = definition.controller || function () {};
+  definition.channels = definition.channels || [];
+
+  if (!definition.name) {
+    throw new Error('You tried to create a screen without name specified!');
+  }
+
+  if (!definition.template) {
+    throw new Error('You tried to create a screen without template specified!');
+  }
+
+  if (!definition.route) {
+    throw new Error('You tried to create a screen without route specified!');
+  }
+
+  var moduleName = conv.names.ngModule(conv.structureComponents.screen, definition.name);
+
+  return {
+    moduleName: moduleName,
+    moduleDependencies: deps,
+    screenName: definition.name,
+    controller: definition.controller,
+    template: definition.template,
+    route: definition.route,
+    channels: definition.channels
+  };
 };
